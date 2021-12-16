693a0f169753bc30b85fc43a6e6d74f4e67b81ab
Benjamin Day <benday@benday.com>
2021-12-16 09:23:50 -0500

    Creating simple database connection test.

--- test/Benday.ArmDemo1.UnitTests/DatabaseConnectionFixture.cs
+++ test/Benday.ArmDemo1.UnitTests/DatabaseConnectionFixture.cs
@@ -9,12 +9,17 @@ namespace Benday.ArmDemo1.UnitTests;
 public class DatabaseConnectionFixture
 {
     [TestMethod]
-    [Timeout(10000)]
+    // [Timeout(10000)]
     public void ConnectToDatabase()
     {
         Console.WriteLine("asdfasdf");
 
-        var connstr = "server=(localdb)\\MSSQLLocalDB; database=test1234; Integrated Security=True;";
+        // var connstr = "server=(localdb)\\MSSQLLocalDB; database=master; Integrated Security=True;";
+        // var connstr = "server=(localdb)\\.; database=test1234; Integrated Security=True;";
+        // var connstr = @"server=np:\\.\pipe\LOCALDB#SHF1EE43\tsql\query";
+        // var connstr = @"Data Source=(localdb)\dev;Connect Timeout=10;";
+        var connstr = @"server=(localdb)\dev;Connect Timeout=10;database=test123;";
+        // var connstr = @"server=(localdb)\dev;Integrated Security=True;Connect Timeout=10;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         using var conn = new SqlConnection(connstr);
 
