536b6328d556a2875ddcadb75fdc047d50c73346
Benjamin Day <benday@benday.com>
2021-12-17 10:27:36 -0500

    deleted unnecessary projects

--- src/Benday.ArmDemo1.WebUi/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Benday.ArmDemo1.WebUi.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
